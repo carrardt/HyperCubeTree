b76b71962091f19ba990141046b5fdd2365ac1a8
carrardt <thierry.carrard@orange.fr>
2017-07-09 21:09:12 +0200

    try to build a numerical simulation

---
+++ hydro/main.cc
@@ -0,0 +1,47 @@
+#include "AmrConnect.h"
+
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+using namespace AmrReconstruction3;
+
+int main(int argc, char* argv[])
+{
+  // verification du nombre minimal d'arguments
+  if( argc<3 )
+    {
+      cerr<<"Utilisation: "<<argv[0]<<" fichier.lvl fichier.tree"<<endl;
+      return 1;
+    }
+  string levelFileName = argv[1];
+  string treeFileName = argv[2];
+
+  cout<<"<- "<<levelFileName<<endl;
+  ifstream fic_grid( levelFileName.c_str() );
+  if( !fic_grid )
+    {
+      cerr<<"Erreur lecture "<<levelFileName<<endl;
+      return 1;
+    }
+  AmrLevels<float,3> levels;
+  levels.fromBinaryStream(fic_grid);
+  fic_grid.close();
+  levels.toStream(cout); cout<<endl;
+
+  cout<<"<- "<<treeFileName<<endl;
+  ifstream fic_tree( treeFileName.c_str() );
+  if( !fic_tree )
+    {
+      cerr<<"Erreur lecture "<<treeFileName<<endl;
+      return 1;
+    }
+  AmrTree tree;
+  tree.fromBinaryStream(fic_tree);
+  fic_tree.close();
+  tree.toStream(cout); cout<<endl;
+
+	
+
+  return 0;
+}
--- include/AmrTree.h
+++ include/AmrTree.h
@@ -69,76 +69,87 @@ namespace AmrReconstruction3
       nodeLevels[0].nodes[0].nCells = 0;
     }
 
-    template<unsigned int D>
-    inline int allocateNodes(LevelInfo<D>* levels, int depth)
-    {
-      int index = nodeLevels[depth+1].size;
-      int nnodes = levels[depth].grid.gridSize();
-      while( (nodeLevels[depth+1].size+nnodes) > nodeLevels[depth+1].capacity )
+	/*
+	 * Make room for children of one node a specified depth.
+	 * return index of first children, relative to to the specified level storage.
+	 */
+	template<unsigned int D>
+	inline int allocateNodes(LevelInfo<D>* levels, int depth)
 	{
-	  TreeNode* tmp = new TreeNode[nodeLevels[depth+1].capacity*2];
-	  memcpy( tmp, nodeLevels[depth+1].nodes, nodeLevels[depth+1].size * sizeof(TreeNode) );
-	  delete [] nodeLevels[depth+1].nodes;
-	  nodeLevels[depth+1].nodes = tmp;
-	  nodeLevels[depth+1].capacity *= 2;
+		int index = nodeLevels[depth+1].size;
+		int nnodes = levels[depth].grid.gridSize();
+		while( (nodeLevels[depth+1].size+nnodes) > nodeLevels[depth+1].capacity )
+		{
+			TreeNode* tmp = new TreeNode[nodeLevels[depth+1].capacity*2];
+			memcpy( tmp, nodeLevels[depth+1].nodes, nodeLevels[depth+1].size * sizeof(TreeNode) );
+			delete [] nodeLevels[depth+1].nodes;
+			nodeLevels[depth+1].nodes = tmp;
+			nodeLevels[depth+1].capacity *= 2;
+		}
+		// increment du nombre de noeuds
+		nodeLevels[depth+1].size += nnodes;
+		// initialisation des noeuds enfants
+		for(int i=0;i<nnodes;i++)
+		{
+			nodeLevels[depth+1].nodes[index+i].index = -1;
+			nodeLevels[depth+1].nodes[index+i].nCells = 0;
+		}
+		return index;
 	}
-      // increment du nombre de noeuds
-      nodeLevels[depth+1].size += nnodes;
-      // initialisation des noeuds enfants
-      for(int i=0;i<nnodes;i++)
-	{
-	  nodeLevels[depth+1].nodes[index+i].index = -1;
-	  nodeLevels[depth+1].nodes[index+i].nCells = 0;
-	}
-      return index;
-    }
 
-    inline int subNode(int depth, int n, int branch) const
-    {
-#ifdef DEBUG
-      assert( depth>=0 && depth<nLevels && n>=0 && n<nodeLevels[depth].size );
-#endif
-      if( nodeLevels[depth].nodes[n].index==-1 )
+	inline int subNode(int depth, int n, int branch) const
 	{
-	  return -1;
+#		ifdef DEBUG
+			assert( depth>=0 && depth<nLevels && n>=0 && n<nodeLevels[depth].size );
+#		endif
+		if( nodeLevels[depth].nodes[n].index==-1 )
+		{
+			return -1;
+		}
+		else
+		{
+			return nodeLevels[depth].nodes[n].index + branch;
+		}
 	}
-      else
-	{
-	  return nodeLevels[depth].nodes[n].index + branch;
-	}
-    }
 
-    inline bool isLeaf(int depth, int n) const
-    {
-#ifdef DEBUG
-      assert( depth>=0 && depth<nLevels && n>=0 && n<nodeLevels[depth].size );
-#endif
-      return nodeLevels[depth].nodes[n].index == -1 ;
-    }
-
-    template <typename T, unsigned int D>
-      inline int insertCell( Vec<T,D> cellCenter, int cellDepth,
-			     Vec<T,D> origin,
-			     LevelInfo<D>* levelInfo, AmrCellSize<T,D>* levelSize )
-    {
-      int n = 0;
-      int depth = 0;
-      for(; depth<cellDepth; depth++)
+	inline bool isLeaf(int depth, int n) const
 	{
-	  AmrCellSize<T,D> cellSize = levelSize[depth+1];
-	  Grid<D> grid = levelInfo[depth].grid;
-	  Vec<unsigned int,D> cellPos = ( cellCenter - origin ) / cellSize ;
-	  unsigned int branch = grid.branch( cellPos );
-	  origin += ( cellSize * cellPos );
-	  if( nodeLevels[depth].nodes[n].index==-1 )
-	    {
-	      nodeLevels[depth].nodes[n].index = allocateNodes( levelInfo, depth );
-	    }
-	  n = nodeLevels[depth].nodes[n].index + branch;
+#		ifdef DEBUG
+		assert( depth>=0 && depth<nLevels && n>=0 && n<nodeLevels[depth].size );
+#		endif
+		return nodeLevels[depth].nodes[n].index == -1 ;
 	}
-      nodeLevels[depth].nodes[n].nCells++;
-      return n;
-    }
+
+	/*
+	 * Insert a new cell, given a position and the depth in the tree.
+	 * This specifically tailored to transform and unstructured mesh to a tree.
+	 */
+	template <typename T, unsigned int D>
+	inline int insertCell(
+		Vec<T,D> cellCenter,
+		int cellDepth,
+		Vec<T,D> origin,
+		LevelInfo<D>* levelInfo,
+		AmrCellSize<T,D>* levelSize )
+	{
+		int n = 0;
+		int depth = 0;
+		for(; depth<cellDepth; depth++)
+		{
+			AmrCellSize<T,D> cellSize = levelSize[depth+1];
+			Grid<D> grid = levelInfo[depth].grid;
+			Vec<unsigned int,D> cellPos = ( cellCenter - origin ) / cellSize ;
+			unsigned int branch = grid.branch( cellPos );
+			origin += ( cellSize * cellPos );
+			if( nodeLevels[depth].nodes[n].index==-1 )
+			{
+				nodeLevels[depth].nodes[n].index = allocateNodes( levelInfo, depth );
+			}
+			n = nodeLevels[depth].nodes[n].index + branch;
+		}
+		nodeLevels[depth].nodes[n].nCells++;
+		return n;
+	}
 
     template <typename StreamT> inline 
     void toStream(StreamT & out) const
@@ -163,42 +174,48 @@ namespace AmrReconstruction3
 	}
     }
 
-    template <typename StreamT> inline 
-    void toBinaryStream(StreamT & out) const
-    {
-      out.write( (char*)&nLevels , sizeof(int) );
-      for(int i=0;i<nLevels;i++)
+	/*
+	 * Write tree structure to file
+	 */
+	template <typename StreamT> inline 
+	void toBinaryStream(StreamT & out) const
 	{
-	  out.write( (char*)&(nodeLevels[i].size) , sizeof(int) );
-	}
-      for(int i=0;i<nLevels;i++)
-	{
-	  out.write( (char*)nodeLevels[i].nodes , nodeLevels[i].size*sizeof(TreeNode) );
-	}
-    }
-
-    template <typename StreamT> inline 
-    void fromBinaryStream(StreamT & in)
-    {
-      in.read( (char*)&nLevels , sizeof(int) );
-      nodeLevels = new NodeLevel[nLevels];
-
-      int totalSize = 0;
-      for(int i=0;i<nLevels;i++)
-	{
-	  nodeLevels[i].capacity = 0; // empeche tout ajout
-	  in.read( (char*)&(nodeLevels[i].size) , sizeof(int) );
-	  totalSize += nodeLevels[i].size;
+		out.write( (char*)&nLevels , sizeof(int) );
+		for(int i=0;i<nLevels;i++)
+		{
+			out.write( (char*)&(nodeLevels[i].size) , sizeof(int) );
+		}
+		for(int i=0;i<nLevels;i++)
+		{
+			out.write( (char*)nodeLevels[i].nodes , nodeLevels[i].size*sizeof(TreeNode) );
+		}
 	}
 
-      TreeNode* ptr = allNodes = new TreeNode[totalSize];
-      for(int i=0;i<nLevels;i++)
+	/*
+	 * Read tree structure from file
+	 */
+	template <typename StreamT> inline 
+	void fromBinaryStream(StreamT & in)
 	{
-	  nodeLevels[i].nodes = ptr;
-	  in.read( (char*)nodeLevels[i].nodes , nodeLevels[i].size*sizeof(TreeNode) );
-	  ptr += nodeLevels[i].size;
+		in.read( (char*)&nLevels , sizeof(int) );
+		nodeLevels = new NodeLevel[nLevels];
+
+		int totalSize = 0;
+		for(int i=0;i<nLevels;i++)
+		{
+			nodeLevels[i].capacity = 0; // empeche tout ajout
+			in.read( (char*)&(nodeLevels[i].size) , sizeof(int) );
+			totalSize += nodeLevels[i].size;
+		}
+
+		TreeNode* ptr = allNodes = new TreeNode[totalSize];
+		for(int i=0;i<nLevels;i++)
+		{
+			nodeLevels[i].nodes = ptr;
+			in.read( (char*)nodeLevels[i].nodes , nodeLevels[i].size*sizeof(TreeNode) );
+			ptr += nodeLevels[i].size;
+		}
 	}
-    }
 
     int nLevels;
     NodeLevel* nodeLevels;
