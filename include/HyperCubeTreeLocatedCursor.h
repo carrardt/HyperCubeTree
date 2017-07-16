143955def4607dab84fa78140e0f0dcc4be49dbb
carrardt <thierry.carrard@orange.fr>
2017-07-16 17:56:18 +0200

    separate header for located cursor

---
+++ include/HyperCubeTreeLocatedCursor.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "HyperCubeTreeCursor.h"
+#include "HyperCubeTreeCell.h"
+#include "Vec.h"
+
+namespace hct
+{
+
+/*
+A tree traversal cursor adding information about position and size of the traversed cell.
+*/
+template<typename _Tree>
+struct HyperCubeTreeLocatedCursor : public hct::HyperCubeTreeCursor<_Tree>
+{
+	using SuperClass = hct::HyperCubeTreeCursor<_Tree>;
+	using Tree = _Tree;
+	static constexpr unsigned int D = Tree::D;
+	using VecT = hct::Vec<double, D>;
+	using SubdivisionGrid = typename Tree::SubdivisionGrid;
+	using GridLocation = typename Tree::GridLocation;
+
+	// initialization constructors
+	inline HyperCubeTreeLocatedCursor(hct::Vec<double, D> domainSize, hct::HyperCubeTreeCell cell = hct::HyperCubeTreeCell())
+		: SuperClass(cell)
+		, m_origin(0.0)
+		, m_size(domainSize) {}
+
+	// recursion constructor
+	inline HyperCubeTreeLocatedCursor(Tree& tree, HyperCubeTreeLocatedCursor parent, SubdivisionGrid grid, GridLocation childLocation)
+		: SuperClass(tree, parent, grid, childLocation)
+	{
+		m_size = parent.m_size / grid;
+		m_origin = parent.m_origin + childLocation * m_size;
+	}
+
+	VecT m_origin;
+	VecT m_size;
+};
+
+}
