ddb92501ba055e6e2ca463f4c7d230d1b2d6d07e
carrardt <thierry.carrard@orange.fr>
2017-08-08 20:59:37 +0200

    first version of tree reader, not even tested yet

--- include/HyperCubeTreeInput.h
+++ include/HyperCubeTreeInput.h
@@ -1,18 +1,24 @@
 #pragma once
 
 #include <string>
+#include <list>
 
 #include "ScalarFunctionInput.h"
 #include "SimpleSubdivisionScheme.h"
 #include "TreeRefineImplicitSurface.h"
 #include "TreeLevelStorage.h"
+#include "HyperCubeTreeLocatedCursor.h"
 
 namespace hct
 {
 	template<unsigned int D, typename T, typename StreamT>
 	static HyperCubeTree<D, SimpleSubdivisionScheme<D> > 
-	inline read_tree(StreamT& input)
-	{ 
+	inline read_tree(StreamT& input, std::list<TreeLevelArray<T> >& scalars, std::list<TreeLevelArray<Vec<T,D> > >& vectors )
+	{
+		using Tree = HyperCubeTree<D, SimpleSubdivisionScheme<D> >;
+		using TreeCursor = typename Tree::DefaultTreeCursor;
+		using LocatedTreeCursor = HyperCubeTreeLocatedCursor<Tree>;
+
 		std::string token;
 		input >> token;
 		if (token == "dimension")
@@ -37,61 +43,110 @@ namespace hct
 			abort(); 
 		}
 
-		using Tree = HyperCubeTree<D, SimpleSubdivisionScheme<D> >;
-		using TreeCursor = typename Tree::DefaultTreeCursor;
-		Tree tree;
+		Tree tree(levels);
 
 		input >> token;
-		while (token == "refine")
+		if (token == "refine")
 		{
-			input >> token;
-			if (token == "level")
+			while (token == "refine")
 			{
-				size_t l = 0;
-				input >> l;
-				assert(l >= 0 && l < tree.getNumberOfLevels());
-				tree.parseLeaves( [l](const TreeCursor& cursor)
+				input >> token;
+				if (token == "level")
 				{
-					if (tree.isRefinable(cursor.cell()) && cursor.cell().level() == l)
+					size_t l = 0;
+					input >> l;
+					assert(l >= 0 && l < tree.getNumberOfLevels());
+					tree.parseLeaves([&tree,l](const TreeCursor& cursor)
 					{
-						tree.refine(cursor.cell());
-					}
-				});
+						if (tree.isRefinable(cursor.cell()) && cursor.cell().level() == l)
+						{
+							tree.refine(cursor.cell());
+						}
+					});
+				}
+				else if (token == "surface")
+				{
+					size_t maxLevel = 0;
+					input >> maxLevel;
+					auto surf = scalar_function_read<D, T>(input);
+					tree_refine_implicit_surface(tree, surf, maxLevel);
+				}
+				else
+				{
+					abort();
+				}
+				// the may be several successive refine directives
+				input >> token;
 			}
-			else if (token == "surface")
-			{
-				size_t maxLevel = 0;
-				input >> maxLevel;
-				auto surf = scalar_function_read<D, T>(input);
-				tree_refine_implicit_surface(tree, surf, maxLevel);
-			}
-			else
-			{
-				abort();
-			}
-			// the may be several successive refine directives
-			input >> token;
+		}
+		else if (token == "structure")
+		{
+			assert("'structure' keyword not handled" && false);
+		}
+		else
+		{
+			abort();
 		}
 
 		while (token == "scalar" || token == "gradient" /*|| token=="vector"*/ )
 		{
-			std::string name;
+			std::string name, functionOrData;
 			input >> name;
+			input >> functionOrData;
 			if (token == "scalar")
 			{
-
+				TreeLevelArray<T> scalarField;
+				scalarField.setName(name);
+				tree.addArray(&scalarField);
+				if (functionOrData == "function")
+				{
+					auto f = scalar_function_read<D, T>(input);
+					tree.preorderParseCells( [&scalarField,f](const LocatedTreeCursor& cursor)
+					{
+						scalarField[cursor.cell()] = f( cursor.position().addHalfUnit().normalize() ).value();
+					}, LocatedTreeCursor() );
+				}
+				else // if(functionOrData=="data")
+				{
+					abort();
+				}
+				scalars.push_back(scalarField);
 			}
 			else if( token=="gradient" )
 			{
-
+				TreeLevelArray< Vec<T,D> > vectorField;
+				vectorField.setName(name);
+				tree.addArray(&vectorField);
+				if (functionOrData == "function")
+				{
+					auto f = scalar_function_read<D, T>(input);
+					tree.preorderParseCells([&vectorField, f](const LocatedTreeCursor& cursor)
+					{
+						vectorField[cursor.cell()] = f(cursor.position().addHalfUnit().normalize()).gradient();
+					}, LocatedTreeCursor());
+				}
+				else // if(functionOrData=="data")
+				{
+					abort();
+				}
 			}
-			else
+			else // if(token=="vector")
 			{
 				abort();
 			}
-
 			input >> token;
 		}
+
+		if (token == "end")
+		{
+			// finalize actions here ?
+		}
+		else
+		{
+			abort();
+		}
+
+		return tree;
 	}
 
 }
--- tests/TestTreeInput.cc
+++ tests/TestTreeInput.cc
@@ -1,7 +1,6 @@
 #include "HyperCubeTree.h"
 #include "SimpleSubdivisionScheme.h"
 #include "HyperCubeTreeInput.h"
-#include "vtkLegacyExportDual.h"
 
 #include <iostream>
 #include <fstream>
@@ -22,11 +21,7 @@ std::ostream& operator << (std::ostream& out, Vec3d p)
 int main(int argc, char* argv[])
 {
 	std::string inputFileName = std::string(HCT_DATA_DIR) + "/deathstar3d_4levels.hct";
-	std::string outputFileName = "output.vtk";
-
 	if (argc >= 2) { inputFileName = argv[1]; }
-	if (argc >= 3) { outputFileName = argv[2]; }
-
 	std::ifstream input(inputFileName);
 	if (!input)
 	{
@@ -40,15 +35,6 @@ int main(int argc, char* argv[])
 	std::list<hct::TreeLevelArray<hct::Vec<double,3> > > vectors;
 	Tree tree = hct::read_tree<3,double>(input,scalars,vectors);
 	tree.toStream(std::cout);
-
-	std::ofstream output(outputFileName);
-	if (!output)
-	{
-		std::cerr << "Error opening file '" << outputFileName << "'" << std::endl;
-		return 1;
-	}
-	std::cout<<"output dual unstructured grid to "<< outputFileName << std::endl;
-	hct::vtk::exportDualUnstructuredGrid(tree, output);
 
 	return 0;
 }
